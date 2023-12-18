3378344650610d09b0ab42e261bb22c85d45a22a
MoXuToS <moxutosic@gmail.com>
2023-12-18 20:40:35 +0300

    main

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,7 @@
 var express = require('express');
 const adminController = require("../Controller/adminController.js");
 var router = express.Router();
-var bodyparser = require('body-parser');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 const jsonParser = express.json();
 
