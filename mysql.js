83f5f8b2392f217d48c304d9043b5cf59b69799f
MoXuToS <moxutosic@gmail.com>
2023-12-18 20:27:06 +0300

    main

--- mysql.js
+++ mysql.js
@@ -1,5 +1,4 @@
 const mysql = require("mysql2");
-const { Connection } = require("mysql2/typings/mysql/lib/Connection");
 Connection = mysql.createConnection({
     host: "localhost",
     user: "root",
