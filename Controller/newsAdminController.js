a22b8780c3b555174c1c497caa019d6e3918e2df
MoXuToS <moxutosic@gmail.com>
2023-12-19 12:00:32 +0300

    main

--- Controller/newsAdminController.js
+++ Controller/newsAdminController.js
@@ -2,27 +2,27 @@ const Models = require("../Models/adminModel.js");
 
 exports.getArticleAll = async function(req, res){
     let m=await Models.getAll(); 
-    res.render('adminArticles', {data:m});
+    res.render('adminNews', {data:m});
 };
 
 exports.getArticle = async function(req, res){ 
     let m=await Models.getOne(req.params.idArticle); 
-    res.render('adminArticleShow', {data:m});
+    res.render('adminNewsShow', {data:m});
 };
 exports.addArticle = async function(req, res){
     await Models.addOne(req.body); 
     let m=await Models.getAll(); 
-    res.render('adminArticles', {data:m});
+    res.render('adminNews', {data:m});
 };
 exports.editArticle = async function(req, res){
     await Models.editOne(req.body); 
     let m=await Models.getOne(req.body.idArticle); 
-    res.render('adminArticleShow', {data:m});
+    res.render('adminNewshow', {data:m});
 };
 exports.deleteArticle = async function(req, res){ 
     // Предполагается, что idArticle передается в запросе
     const idArticle = req.body.idArticle;
     await Models.deleteOne(idArticle); 
     let m=await Models.getAll();
-    res.render('adminArticles', {data:m});
+    res.render('adminNews', {data:m});
 };
